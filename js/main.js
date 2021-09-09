c9c1942a6dfc251a51fc02c32489055bf787c6fa
rkterungwa16 <kombolalbert@gmail.com>
2021-09-09 12:25:35 +0100

    Center component

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ button.addEventListener('click', toggle);
 function toggle(){
     if(smNav.classList.contains('is-open')){
         smNav.classList.remove('is-open');
+        button.classList.remove('is-dark');//toggle button background
     }else {
         smNav.classList.add('is-open');
+        button.classList.add('is-dark');//toggle button background
     }
 }
